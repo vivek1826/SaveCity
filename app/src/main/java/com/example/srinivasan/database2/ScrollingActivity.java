0904f2cbaa4e2f9d1e48cbd3a7743b49869c5f19
vivek1826 <vivek_96@rocketmail.com>
2017-04-24 00:11:13 +0530

    updated

--- app/src/main/java/com/example/srinivasan/database2/ScrollingActivity.java
+++ app/src/main/java/com/example/srinivasan/database2/ScrollingActivity.java
@@ -1,25 +1,59 @@
 package com.example.srinivasan.database2;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 public class ScrollingActivity extends AppCompatActivity {
-
+    Button sub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        sub= (Button)findViewById(R.id.feedsub);
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new AlertDialog.Builder(ScrollingActivity.this)
+                        .setTitle("     Feedback")
+                        .setMessage("Your Response is recorded \n Thank You")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Whatever...
+                                Intent i = new Intent(ScrollingActivity.this,Navmenu.class);
+                                startActivity(i);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+            }
+        });
 
     }
 
